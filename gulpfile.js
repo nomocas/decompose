b44a3479a19e3f9b3b6c0784202c7643e3b1696c
Gilles Coomans <gilles.coomans@gmail.com>
2015-05-15 12:50:07 +0200

    add linting in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,20 @@ var gulp = require('gulp'),
     uglify = require('gulp-uglifyjs'),
     rename = require("gulp-rename");
 //___________________________________________________
-gulp.task('default', ['uglify']);
+
+gulp.task('default', ['lint', 'uglify']);
+gulp.task('lint', ['jslint']);
+
+//___________________________________________________
+// npm i --save-dev gulp-jshint jshint-stylish
+var jshint = require('gulp-jshint'),
+    stylish = require('jshint-stylish');
+
+gulp.task('jslint', function() {
+    gulp.src('./index.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter(stylish));
+});
 //___________________________________________________
 gulp.task('serve-test', function() {
     var server = gls.static("./test", 8287);
